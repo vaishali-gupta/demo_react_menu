fc3f8af4b21d53c0203071f7c20cf180063e506b
Vaishali Gupta <vaishaligupta0205@gmail.com>
2020-04-01 14:16:13 -0500

    Adding some text in paragraph for the page to look good

--- src/App.js
+++ src/App.js
@@ -21,10 +21,52 @@ class App extends Component {
             <h1>
               <a href="https://github.com/negomi/react-burger-menu">react-burger-menu</a>
             </h1>
-            <a className="side-button left active">Left</a>
-            <a className="side-button right">Right</a>
+           
             <h2 className="description">An off-canvas sidebar React component with a collection of effects and styles using CSS transitions and SVG path animations.</h2>
-            
+            <p>Lorem ipsum dolor sit amet, temporibus dissentiet sit te, 
+              vim wisi adipisci disputando cu. An sed dicat feugiat. 
+              Ex diam maluisset sententiae sed, mei dico facer philosophia ne. 
+              Eu clita facilisi eum, sumo duis signiferumque eam et.</p>
+
+            <p>Eum ad nobis verterem consetetur. Ea quo aperiam lucilius, vix et diam interpretaris. Nibh 
+              sanctus ornatus an nam, tota aliquip legimus vix ad, sed ut esse epicuri quaestio. Brute
+               prodesset nam at, te ludus accommodare duo, ei prima dolorum definitionem sea. Elitr 
+               munere voluptatibus in sed, saepe fierent no usu, eu quis modo lobortis nam.
+                An usu docendi nostrum.</p>
+
+            <hr></hr>
+            <p>Lorem ipsum dolor sit amet, temporibus dissentiet sit te, 
+              vim wisi adipisci disputando cu. An sed dicat feugiat. 
+              Ex diam maluisset sententiae sed, mei dico facer philosophia ne. 
+              Eu clita facilisi eum, sumo duis signiferumque eam et.</p>
+
+            <p>Eum ad nobis verterem consetetur. Ea quo aperiam lucilius, vix et diam interpretaris. Nibh 
+              sanctus ornatus an nam, tota aliquip legimus vix ad, sed ut esse epicuri quaestio. Brute
+               prodesset nam at, te ludus accommodare duo, ei prima dolorum definitionem sea. Elitr 
+               munere voluptatibus in sed, saepe fierent no usu, eu quis modo lobortis nam.
+                An usu docendi nostrum.</p>
+
+                <hr></hr>
+            <p>Lorem ipsum dolor sit amet, temporibus dissentiet sit te, 
+              vim wisi adipisci disputando cu. An sed dicat feugiat. 
+              Ex diam maluisset sententiae sed, mei dico facer philosophia ne. 
+              Eu clita facilisi eum, sumo duis signiferumque eam et.</p>
+              <p>Eum ad nobis verterem consetetur. Ea quo aperiam lucilius, vix et diam interpretaris. Nibh 
+              sanctus ornatus an nam, tota aliquip legimus vix ad, sed ut esse epicuri quaestio. Brute
+               prodesset nam at, te ludus accommodare duo, ei prima dolorum definitionem sea. Elitr 
+               munere voluptatibus in sed, saepe fierent no usu, eu quis modo lobortis nam.
+                An usu docendi nostrum.</p>
+
+                <hr></hr>
+            <p>Lorem ipsum dolor sit amet, temporibus dissentiet sit te, 
+              vim wisi adipisci disputando cu. An sed dicat feugiat. 
+              Ex diam maluisset sententiae sed, mei dico facer philosophia ne. 
+              Eu clita facilisi eum, sumo duis signiferumque eam et.</p>
+              <p>Eum ad nobis verterem consetetur. Ea quo aperiam lucilius, vix et diam interpretaris. Nibh 
+              sanctus ornatus an nam, tota aliquip legimus vix ad, sed ut esse epicuri quaestio. Brute
+               prodesset nam at, te ludus accommodare duo, ei prima dolorum definitionem sea. Elitr 
+               munere voluptatibus in sed, saepe fierent no usu, eu quis modo lobortis nam.
+                An usu docendi nostrum.</p>
           </main>
       </div>
       
