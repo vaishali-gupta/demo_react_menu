d0f5f4379e43338ba0130dd042ed1d5ffb7ef3f4
Vaishali Gupta <vaishaligupta0205@gmail.com>
2020-04-01 14:01:23 -0500

    Commiting the React burger Menu Demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
+import { scaleRotate as Menu } from 'react-burger-menu';
+class App extends Component {
+  showSettings (event) {
+    event.preventDefault();
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="outer-container">
+      <Menu  pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
+        
+          <a id="home" className="menu-item" href="/">Home</a>
+          <a id="about" className="menu-item" href="/about">About</a>
+          <a id="contact" className="menu-item" href="/contact">Contact</a>
+          <a onClick={ this.showSettings } className="menu-item" href="">Settings</a>
+       
+        </Menu>
+
+        <main id="page-wrap">
+            <h1>
+              <a href="https://github.com/negomi/react-burger-menu">react-burger-menu</a>
+            </h1>
+            <a className="side-button left active">Left</a>
+            <a className="side-button right">Right</a>
+            <h2 className="description">An off-canvas sidebar React component with a collection of effects and styles using CSS transitions and SVG path animations.</h2>
+            
+          </main>
+      </div>
+      
   );
+  }
 }
+
 
 export default App;
